5e051c90d49f3dff1946b6512ef28a3efa91ea9e
AlonAzran <127650722+AlonAzran@users.noreply.github.com>
2024-12-25 13:39:52 +0200

    123

---
+++ frontend/src/local_functions_mock.js
@@ -0,0 +1,19 @@
+    function MockGetPatientPredictXGBOOST(id) {
+      return 86; // Return example mortality percentage
+    }
+
+    function MockGetPatientPredictDecisionTree(id) {
+      return 20; // Return example mortality percentage
+    }
+
+    function MockGetPatientPredictLogisticRegression(id) {
+      return 64; // Return example mortality percentage
+    }
+
+    function MockGetPatientDeatails(id) {
+      return 64; // Return example mortality percentage
+    }
+
+    function MockGetPatientExplanaition(id) {
+      return 64; // Return example mortality percentage
+    }
